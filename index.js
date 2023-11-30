e16c673dd307799261f05acf7b5a032dbd40ae3c
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2023-12-01 00:20:43 +0700

    handle cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const PORT = process.env.PORT || 8000; 
 const routes = require("./routes");
 
+app.use(cors())
 app.use(express.json());
+
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
